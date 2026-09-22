3a6528d14e4200f315cda0a4a0e9d4e4da7def02
Ricardo Calipimbuth Mendes da Costa <ricascosta1@gmail.com>
2026-09-23 00:32:15 +0100

    ex001: Criação de classe, Método Construtor, Atributos, Instância e Objetos

---
+++ ex001/ex001.py
@@ -0,0 +1,31 @@
+#Declaração dde classe
+
+class Gafanhoto:
+    def __init__(self): # Método construtor
+        # Atributos de Instância
+        self.nome = ""
+        self.idade = 0
+
+
+    # Método de Instaancia
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+
+#Declaração de Objetos
+g1 = Gafanhoto()
+g1.nome = "Maria"
+g1.idade = 17
+g1.aniversario()
+print(g1.mensagem())
+
+g2 = Gafanhoto()
+g2.nome = "Mauro"
+g2.idade = 53
+g2.aniversario()
+print(g2.mensagem())
+
+g3 = Gafanhoto()
+print(g3.mensagem())
